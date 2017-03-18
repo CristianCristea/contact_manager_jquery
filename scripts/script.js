c6dba4cd3f44249b9d0e6373f3195ebad2036326
Cristian Cristea <cristiancristea.1987@gmail.com>
2017-03-18 21:38:38 +0100

    Add functiality for edit, delete contact

--- scripts/script.js
+++ scripts/script.js
@@ -6,36 +6,96 @@
 // on form submit append the new created contact
 // todo use localStorage
 
-$(function() {
-  $('#add_contact_btn').on('click', function(e) {
-    e.preventDefault();
+var app = {
+  showForm: function() {
     $('main').slideUp(300);
     $('.add-contact-form').slideDown(400);
-  });
-
-   $('button[type="reset"]').on('click', function(e) {
-    e.preventDefault();
+  },
+  hideForm: function() {
     $('main').slideDown(400);
     $('.add-contact-form').slideUp(300);
-  });
+  },
+  getContactData: function(e) {
+    // implement edit - get data from li, recall form prepopulated with data, rerewrite li
+    var $li = $(e.currentTarget).closest('li');
+    var nameData = $li.find('.name').text();
+    var emailData = $li.find('.email').text();
+    var addressData = $li.find('.address').text();
+    $('.add-contact-form form').addClass("editing");
+    $('#name').val(nameData);
+    $('#email').val(emailData);
+    $('#address').val(addressData);
+    $('#current_id').attr('data-id', $li.attr("data-id"));
+    this.showForm();
+  },
+  editContact: function(e, id) {
+    e.preventDefault();
+    var $li = $('li').filter(function() {
+    return ($(this).attr('data-id') == id);
+    });
+    var $form = $('.add-contact-form form');
+    var nameData = $form.find('#name').val();
+    var emailData = $form.find('#email').val();
+    var addressData = $form.find('#address').val();
 
-   $('.add-contact-form form').submit(function(e) {
+    $li.find('.name').text(nameData); 
+    $li.find('.email').text(emailData); 
+    $li.find('.address').text(addressData); 
+  },
+  deleteContact: function(e, id) {
+    var $li = $('li').filter(function() {
+      return ($(this).attr('data-id') == id);
+    });
+    $li.remove();
+  },
+  clearForm: function(e) {
+    $(e.target).find('input').not(':hidden').val('');
+    $(e.target).removeClass("editing");
+  },
+  newContact: function() {
+    var contactTemplate = $('#contact').html();
+    var contact = $('.add-contact-form form').serializeArray();
+    var templateFunction = Handlebars.compile(contactTemplate);
+    var html_code = templateFunction({contact: contact}); 
+    $('#contacts').append(html_code);
+    // console.log(contact);
+  },
+  incrementId: function(id) {
+    return Number(++id);
+  },
+  init: function() {
+    var self = this;
+    var $form =  $('.add-contact-form form');
+
+    $('#add_contact_btn').on('click', this.showForm);
+    $('button[type="reset"]').on('click', this.hideForm);
+
+    $form.submit(function(e) {
       e.preventDefault();
-      var contactTemplate = $('#contact').html();
-      var contact = $(this).serializeArray();
-      var templateFunction = Handlebars.compile(contactTemplate);
-      var html_code = templateFunction({contact: contact}); 
-      console.log(html_code);
-      $('#contacts').append(html_code);
+      var form_id = $('input[type="hidden"]').val();
+      var li_id = $form.find('#current_id').attr('data-id');
 
-      $('main').slideDown(400);
-      $('.add-contact-form').slideUp(300);
+      if ( $form.hasClass("editing")) {
+        self.editContact(e, li_id);
+      } else {
+        $('input[type="hidden"]').val(self.incrementId(form_id));
+        self.newContact(e);
+      } 
+      self.hideForm();
+      self.clearForm(e);
+    });
 
-      $(this).find('input').val('');
-   });
+    $('#contacts').on('click', '.edit', function(e) {
+      self.getContactData(e);
+    });
 
-   // todo
-   // implement delete - get ul elem - identify index remove from DOM
-   // implement edit - get data from li, recall form prepopulated with data, rerewrite li
-   // implement search get all names - array, get input search, check with the array elem, return match
+    $('#contacts').on('click', '.delete', function(e) {
+      var id = $(e.currentTarget).closest('li').attr('data-id');
+      self.deleteContact(e, id);
+    });
+  }
+};
+
+$(function() {
+  app.init();
 });
